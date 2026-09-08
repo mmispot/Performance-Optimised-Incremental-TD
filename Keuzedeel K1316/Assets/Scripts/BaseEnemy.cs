a66dda12b351a963f3adc3ac55c38adab02a59b5
Emilia Beeke <emiliadeboer1@gmail.com>
2026-09-08 14:20:01 +0200

    feat: enemy follows path and despawns at the end

---
+++ Keuzedeel K1316/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+
+public class BaseEnemy : MonoBehaviour
+{
+    public Transform[] pathPoints;
+    public Transform spawner;
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    private void Start()
+    {
+        SpawnIn();
+    }
+
+    private void SpawnIn()
+    {
+        transform.position = spawner.position;
+        gameObject.SetActive(true);
+
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        foreach (Transform targetPoint in pathPoints) // loop through each point in the path (transforms in scene)
+        {
+            while (Vector3.Distance(transform.position, targetPoint.position) > 0.01f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+        }
+
+        if (Vector3.Distance(transform.position, pathPoints[10].position) < 0.01f)
+        {
+            DeSpawn();
+        }    
+    }
+
+    private void DeSpawn()
+    {
+        Debug.Log("Enemy reached the end of the path and is despawning.");
+        gameObject.SetActive(false);
+    }
+}
